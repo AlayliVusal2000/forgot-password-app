9e006d4cb65b3a44b04717ab3cd4157232c682cb
AlayliVusal2000 <alayli2000@mail.ru>
2023-10-26 11:54:34 +0400

    Add forgot-password app first structures

---
+++ src/main/java/az/forgotpasswordapp/ForgotPasswordAppApplication.java
@@ -0,0 +1,13 @@
+package az.forgotpasswordapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForgotPasswordAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ForgotPasswordAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/az/forgotpasswordapp/ForgotPasswordAppApplicationTests.java
@@ -0,0 +1,13 @@
+package az.forgotpasswordapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForgotPasswordAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
